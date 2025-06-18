d10685e7ecac9d505cc2b6f6befd3a734025e2fa
kengnidoriane <dorianekengni@gmail.com>
2025-06-18 17:01:16 +0200

    feature: add task model

---
+++ src/main/java/com/todolist/toDoList/model/Task.java
@@ -0,0 +1,42 @@
+package com.todolist.toDoList.model;
+
+//import jakarta.persistence.*;
+//import lombok.*;
+//import org.hibernate.annotations.CreationTimestamp;
+//import org.hibernate.annotations.UpdateTimestamp;
+//import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
+enum TaskStatus { TODO, IN_PROGRESS, DONE }
